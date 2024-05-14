50e7f3841fc926a8a5aadc1949c8f74229f731fe
LautaroFavoni <lautarocarp4@hotmail.com>
2024-05-14 18:34:06 -0300

    previo a base de datos

--- src/main/java/com/example/practicaClase/persintence/entities/Admin.java
+++ src/main/java/com/example/practicaClase/persintence/entities/Admin.java
@@ -17,8 +17,31 @@ public class Admin {
     @Column(nullable = false)
     private String password;
 
+    // Getter y Setter para el campo id
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    // Getter y Setter para el campo name
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    // Getter y Setter para el campo password
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
+
